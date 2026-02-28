68488d70529dc7a3649436d820886d7a356bfda4
zymbalevskyi <eu.zymbalevskyi@gmail.com>
2026-02-28 15:34:19 +0200

    Initial Commit

---
+++ ScreenoRiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenoRiz
+//
+//  Created by Yevhen on 28.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenoRiz/ScreenoRizApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenoRizApp.swift
+//  ScreenoRiz
+//
+//  Created by Yevhen on 28.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct ScreenoRizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
